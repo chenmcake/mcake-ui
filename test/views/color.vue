3a163343fb4da4c1154d924771ae8823a4424e81
chenyuebin <568474678@qq.com>
2018-10-12 18:19:47 +0800

    增加文字和背景色彩主题样式

--- test/views/color.vue
+++ test/views/color.vue
@@ -1,5 +1,4 @@
 <style lang="scss" scoped>
-@import "../../src/styles/variables";
 
 .demo-tit {
     padding-left: 2.5%;
@@ -7,32 +6,25 @@
 
 // 文字颜色
 .text-demo {
+    margin-bottom: 20px;
+
 }
 .text {
     font-size: 18px;
     margin-right: 20px;
     padding-left: 2.5%;
 }
-.text-primary {
-    color: $color-primary;
-}
-.text-success {
-    color: $color-success;
-}
-.text-info {
-    color: $color-info;
-}
-.text-warning {
-    color: $color-warning;
-}
-.text-error {
-    color: $color-error;
-}
-.text-noble {
-    color: $color-noble;
-}
 
 // 背景颜色
+.bg-demo {
+    p {
+        font-size: 16px;
+        font-weight: 400;
+        margin-top: 10px;
+        margin-left: 2.5%;
+    }
+}
+
 .color-item {
     width: 30%;
     height: 70px;
@@ -45,65 +37,7 @@
     color: #fff;
 }
 
-.primary {
-    background-color: $color-primary;
-}
-.primary-light {
-    background-color: lighten($color-primary, $color-gradient);
-}
-.primary-dark {
-    background-color: darken($color-primary, $color-gradient);
-}
 
-.success {
-    background-color: $color-success;
-}
-.success-light {
-    background-color: lighten($color-success, $color-gradient);
-}
-.success-dark {
-    background-color: darken($color-success, $color-gradient);
-}
-
-.info {
-    background-color: $color-info;
-}
-.info-light {
-    background-color: lighten($color-info, $color-gradient);
-}
-.info-dark {
-    background-color: darken($color-info, $color-gradient);
-}
-
-.warning {
-    background-color: $color-warning;
-}
-.warning-light {
-    background-color: lighten($color-warning, $color-gradient);
-}
-.warning-dark {
-    background-color: darken($color-warning, $color-gradient);
-}
-
-.error {
-    background-color: $color-error;
-}
-.error-light {
-    background-color: lighten($color-error, $color-gradient);
-}
-.error-dark {
-    background-color: darken($color-error, $color-gradient);
-}
-
-.noble {
-    background-color: $color-noble;
-}
-.noble-light {
-    background-color: lighten($color-noble, $color-gradient);
-}
-.noble-dark {
-    background-color: darken($color-noble, $color-gradient);
-}
 </style>
 <template>
     <div class="color-wrap">
@@ -111,41 +45,49 @@
             <div class="demo-tit">
                 <h3>文字颜色</h3>
             </div>
-            <span class="text text-primary">primary</span>
-            <span class="text text-success">success</span>
-            <span class="text text-info">info</span>
-            <span class="text text-warning">warning</span>
-            <span class="text text-error">error</span>
-            <span class="text text-noble">noble</span>
+            <div class="demo-cont">
+                <span class="text m-text-primary">primary</span>
+                <span class="text m-text-success">success</span>
+                <span class="text m-text-info">info</span>
+                <span class="text m-text-warning">warning</span>
+                <span class="text m-text-error">error</span>
+                <span class="text m-text-noble">noble</span>
+            </div>
         </div>
-        <div class="bg-demo clear">
+        <div class="bg-demo">
             <div class="demo-tit">
                 <h3>背景颜色</h3>
             </div>
-            <div class="color-item primary">primary</div>
-            <div class="color-item primary-light">primary-light</div>
-            <div class="color-item primary-dark">primary-dark</div>
+            <div class="demo-cont clear">
+                <div class="color-item m-bg-primary">primary</div>
+                <div class="color-item m-bg-primary-light">primary-light</div>
+                <div class="color-item m-bg-primary-dark">primary-dark</div>
 
-            <div class="color-item success">success</div>
-            <div class="color-item success-light">success-light</div>
-            <div class="color-item success-dark">success-dark</div>
+                <div class="color-item m-bg-success">success</div>
+                <div class="color-item m-bg-success-light">success-light</div>
+                <div class="color-item m-bg-success-dark">success-dark</div>
 
-            <div class="color-item info">info</div>
-            <div class="color-item info-light">info-light</div>
-            <div class="color-item info-dark">info-dark</div>
+                <div class="color-item m-bg-info">info</div>
+                <div class="color-item m-bg-info-light">info-light</div>
+                <div class="color-item m-bg-info-dark">info-dark</div>
 
-            <div class="color-item warning">warning</div>
-            <div class="color-item warning-light">warning-light</div>
-            <div class="color-item warning-dark">warning-dark</div>
+                <div class="color-item m-bg-warning">warning</div>
+                <div class="color-item m-bg-warning-light">warning-light</div>
+                <div class="color-item m-bg-warning-dark">warning-dark</div>
 
-            <div class="color-item error">error</div>
-            <div class="color-item error-light">error-light</div>
-            <div class="color-item error-dark">error-dark</div>
+                <div class="color-item m-bg-error">error</div>
+                <div class="color-item m-bg-error-light">error-light</div>
+                <div class="color-item m-bg-error-dark">error-dark</div>
 
-            <div class="color-item noble">noble</div>
-            <div class="color-item noble-light">noble-light</div>
-            <div class="color-item noble-dark">noble-dark</div>
+                <div class="color-item m-bg-noble">noble</div>
+                <div class="color-item m-bg-noble-light">noble-light</div>
+                <div class="color-item m-bg-noble-dark">noble-dark</div>
+            </div>
+
+            <p class="m-text-error">注意：可以通过设置class为m-text-{theme} 或 m-bg-{theme}来定义颜色主题</p>
         </div>
+
+
     </div>
 </template>
 <script>
