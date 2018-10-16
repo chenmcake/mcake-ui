90fba6fe59392ea1532be5d4151f6ade1abdfc67
chenyuebin <568474678@qq.com>
2018-10-16 15:50:40 +0800

    完成InputNumber组件加减操作

--- src/components/input-number/input-number.vue
+++ src/components/input-number/input-number.vue
@@ -1,9 +1,9 @@
 <template>
     <div :class="wrapClasses">
         <!-- 减 -->
-        <span :class="reduceClasses"><i class="m-i-reduce"></i></span>
+        <span :class="reduceClasses" @click="reduce"><i class="m-i-reduce"></i></span>
         <!-- 加 -->
-        <span :class="plusClasses"><i class="m-i-plus"></i></span>
+        <span :class="plusClasses" @click="plus"><i class="m-i-plus"></i></span>
         <!-- 输入框 -->
         <input
             class="m-ipt-number"
@@ -30,19 +30,19 @@ export default {
     props: {
         // 默认值
         value: {
-            type: [Number, String],
+            type: Number,
             default: 0
         },
         // 最大值
-        // max: {
-        //     type: Number,
-        //     default: Infinity
-        // },
+        max: {
+            type: Number,
+            default: Infinity
+        },
         // 最小值
-        // min: {
-        //     type: Number,
-        //     default: -Infinity
-        // },
+        min: {
+            type: Number,
+            default: -Infinity
+        },
         // 间隔值 每次递增或递减的数目
         step: {
             type: Number,
@@ -53,7 +53,7 @@ export default {
             type: Boolean,
             default: false
         },
-        // 精度 用于控制小数位数
+        // 精度 用于控制小数位数 值必须是一个非负整数，并且不能小于 step 的小数位数。
         precision: Number,
         // name 用于表单提交
         name: String,
@@ -73,6 +73,9 @@ export default {
     watch: {
         value(val) {
             this.currentValue = val;
+        },
+        currentValue (val) {
+            this.changeValue(val);
         },
     },
     computed: {
@@ -116,6 +119,23 @@ export default {
             else {
                 return this.precisionValue;
             }
+        },
+        // 精度小数位数
+        numPrecision() {
+            const { value, step, getPrecision, precision } = this;
+            const stepPrecision = getPrecision(step);
+
+            // 如果精度存在 返回精度
+            if (precision !== undefined) {
+                if (stepPrecision > precision) {
+                    console.warn('[McakeUI Warn][InputNumber] 精度(precision)不得小于步数(step)的小数位数');
+                }
+                return precision;
+            }
+            // 如果不存在 取当前值或步数的精度
+            else {
+                return Math.max(getPrecision(value), stepPrecision);
+            }
         }
     },
     mounted() {
@@ -128,15 +148,81 @@ export default {
             if (!isNaN(val)) {
                 const step = this.step;
 
-                this.plusDisabled = val + step > this.max;
-                this.reduceDisabled = val - step < this.min;
+                this.plusDisabled = this.disabled || val + step > this.max;
+                this.reduceDisabled = this.disabled || val - step < this.min;
             } else {
                 this.plusDisabled = true;
                 this.reduceDisabled = true;
             }
         },
+        // 加
+        plus() {
+            // 如果为禁止点击
+            if(this.disabled || this.plusDisabled) return;
+            // 输入框的值
+            const value = this.value || 0;
+            // 进行加法运算
+            const newVal = this._plus(value, this.step);
+            // 设置新的值
+            this.setCurrentValue(newVal);
+        },
+        _plus(value, step) {
+            if (typeof value !== 'number' && value !== undefined) return this.currentValue;
+            const precisionFactor = Math.pow(10, this.numPrecision);
+            // 将数字转成整数运算后在处理小数精度 解决小数运算问题
+            return this.toPrecision((precisionFactor * value + precisionFactor * step) / precisionFactor);
+        },
+        // 减
+        reduce() {
+            // 如果为禁止点击
+            if(this.disabled || this.reduceDisabled) return;
+            // 输入框的值
+            const value = this.value || 0;
+            // 进行加法运算
+            const newVal = this._reduce(value, this.step);
+            // 设置新的值
+            this.setCurrentValue(newVal);
+        },
+        _reduce(value, step) {
+            if (typeof value !== 'number' && value !== undefined) return this.currentValue;
+            const precisionFactor = Math.pow(10, this.numPrecision);
+            // 将数字转成整数运算后在处理小数精度 解决小数运算问题
+            return this.toPrecision((precisionFactor * value - precisionFactor * step) / precisionFactor);
+        },
+        setCurrentValue(newVal) {
+            // 获取之前的值
+            const oldVal = this.currentValue;
+            // 处理精度
+            if (typeof newVal === 'number' && this.precision !== undefined) {
+                newVal = this.toPrecision(newVal, this.precision);
+            }
+            // 处理最大值和最小值
+            if (newVal >= this.max) newVal = this.max;
+            if (newVal <= this.min) newVal = this.min;
+            if (oldVal === newVal) return;
+
+            this.$emit('input', newVal);
+            this.$emit('on-change', newVal, oldVal);
+            this.currentValue = newVal;
+        },
         // 点击事件
         handleClick(e) {
+        },
+        // 获取精度小数位数
+        getPrecision(value) {
+          if (value === undefined) return 0;
+          const valueString = value.toString();
+          const dotPosition = valueString.indexOf('.');
+          let precision = 0;
+          if (dotPosition !== -1) {
+            precision = valueString.length - dotPosition - 1;
+          }
+          return precision;
+        },
+        // 按照精度转换值
+        toPrecision(num, precision) {
+          if (precision === undefined) precision = this.numPrecision;
+          return parseFloat(parseFloat(Number(num).toFixed(precision)));
         },
     }
 };
--- test/views/input-number.vue
+++ test/views/input-number.vue
@@ -38,7 +38,7 @@
             </div>
             <div class="demo-item-cont">
                 <div class="demo-item-box">
-                    <m-input-number placeholder="提示信息" />
+                    <m-input-number v-model="value0" :min="-5" :max="10" placeholder="提示信息" />
                 </div>
             </div>
         </div> <!-- demo-item 结束 -->
@@ -49,11 +49,24 @@
             </div>
             <div class="demo-item-cont">
                 <div class="demo-item-box">
-                    <m-input-number :value="1" disabled />
+                    <m-input-number v-model="value1" disabled />
                 </div>
             </div>
         </div> <!-- demo-item 结束 -->
 
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>步数</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box">
+                    <m-input-number v-model="value2" :step="1.25" :precision="2" :min="1" :max="10" />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+
+        <m-button type="primary" @click="getValue">点击</m-button>
 
 
     </div> <!-- demo-wrap 结束 -->
@@ -61,10 +74,22 @@
 <script>
 export default {
     data() {
-        return {}
+        return {
+            value0: 12,
+            value1: 1,
+            value2: 1
+        }
+    },
+    watch: {
+
+        value1(value) {
+            console.log(value)
+        }
     },
     methods: {
-
+        getValue() {
+            console.log(this.value0)
+        }
     }
 }
 </script>
