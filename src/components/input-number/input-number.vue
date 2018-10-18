8877b17d7b3c2379576dbed623a3d9e7e561a63e
chenyuebin <568474678@qq.com>
2018-10-18 17:18:19 +0800

    完成数字输入框所有功能开发

--- src/components/input-number/input-number.vue
+++ src/components/input-number/input-number.vue
@@ -1,23 +1,29 @@
 <template>
     <div :class="wrapClasses">
         <!-- 减 -->
-        <span :class="reduceClasses" @click="reduce"><i class="m-i-reduce"></i></span>
+        <span :class="reduceClasses" @click="reduce"><i :class="controlsAtRight ? 'm-i-bottom' : 'm-i-reduce'"></i></span>
         <!-- 加 -->
-        <span :class="plusClasses" @click="plus"><i class="m-i-plus"></i></span>
+        <span :class="plusClasses" @click="plus"><i :class="controlsAtRight ? 'm-i-top' : 'm-i-plus'"></i></span>
         <!-- 输入框 -->
         <input
+            ref="input"
             class="m-ipt-number"
             type="text"
             :name="name"
             :value="formatterValue"
             :placeholder="placeholder"
-            :disabled="disabled">
+            :disabled="disabled"
+            :readonly="readonly || !editable"
+            @change="handleChange"
+            @keydown.stop="handleKeyDown"
+            @focus="handleFocus"
+            @blur="handleBlur" >
     </div>
 </template>
 
 <script>
 // 引入公共方法
-import { toNumber } from '../../utils/base';
+import { includes, toNumber, isNull } from '../../utils/base';
 // 引入输入框组件
 import MInput from '../input/input.vue';
 // 主容器 class
@@ -28,6 +34,32 @@ export default {
     name: 'MInputNumber',
     components: { MInput },
     props: {
+        // 尺寸
+        size: {
+            type: String,
+            validator (value) {
+                return includes(['mini', 'small', 'large'], value);
+            }
+        },
+        // 形状
+        shape: {
+            type: String,
+            validator (value) {
+                return includes(['fillet'], value);
+            }
+        },
+        // 按钮位置
+        controlsPosition: {
+            type: String,
+            validator (value) {
+                return includes(['right'], value);
+            }
+        },
+        // 是否为长输入框
+        long: {
+            type: Boolean,
+            default: false
+        },
         // 默认值
         value: {
             type: Number,
@@ -58,7 +90,25 @@ export default {
         // name 用于表单提交
         name: String,
         // 提示信息
-        placeholder: String
+        placeholder: String,
+        // 格式化数据的方法
+        formatter: {
+            type: Function
+        },
+        // 将格式化后的字符串转回数字的方法
+        parser: {
+            type: Function
+        },
+        // 只读
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        // 不可编辑
+        editable: {
+            type: Boolean,
+            default: true
+        },
     },
     data() {
         return {
@@ -75,8 +125,17 @@ export default {
             this.currentValue = val;
         },
         currentValue (val) {
-            this.changeValue(val);
+            this.handleBtnStatus(val);
         },
+        disabled(val) {
+            this.handleBtnStatus(this.currentValue);
+        },
+        min() {
+            this.handleBtnStatus(this.currentValue);
+        },
+        max() {
+            this.handleBtnStatus(this.currentValue);
+        }
     },
     computed: {
         // 主容器class
@@ -85,6 +144,15 @@ export default {
                 // 默认class
                 `${wrapClass}`,
                 {
+                    // 尺寸
+                    [`${wrapClass}-${this.size}`]: !!this.size,
+                    // 形状
+                    [`${wrapClass}-${this.shape}`]: !!this.shape,
+                    // 是否为占整行
+                    [`${wrapClass}-long`]: this.long,
+                    // 按钮放置在右边
+                    'controls-right': this.controlsAtRight,
+                    // 禁止
                     'disabled': this.disabled
                 }
             ];
@@ -107,6 +175,8 @@ export default {
         },
         // 根据精度计算当前值
         precisionValue () {
+            // 如果不存在
+            if(!this.currentValue) return this.currentValue;
             // 如果精度存在则根据精度计算 否则使用当前值
             return this.precision ? this.currentValue.toFixed(this.precision) : this.currentValue;
         },
@@ -136,14 +206,18 @@ export default {
             else {
                 return Math.max(getPrecision(value), stepPrecision);
             }
+        },
+        // 按钮是否放置在右边
+        controlsAtRight() {
+            return this.controlsPosition === "right";
         }
     },
     mounted() {
-        this.changeValue(this.currentValue);
+        this.handleBtnStatus(this.currentValue);
     },
     methods: {
-        // 对当前值进行改变
-        changeValue(val) {
+        // 处理按钮禁止状态 根据当前值设置
+        handleBtnStatus(val) {
             val = Number(val);
             if (!isNaN(val)) {
                 const step = this.step;
@@ -158,7 +232,7 @@ export default {
         // 加
         plus() {
             // 如果为禁止点击
-            if(this.disabled || this.plusDisabled) return;
+            if(this.disabled || this.readonly || this.plusDisabled) return;
             // 输入框的值
             const value = this.value || 0;
             // 进行加法运算
@@ -167,7 +241,9 @@ export default {
             this.setCurrentValue(newVal);
         },
         _plus(value, step) {
+            // 如果值不为数字且不为空
             if (typeof value !== 'number' && value !== undefined) return this.currentValue;
+            // 先将值乘以10的小数点位数幂次方
             const precisionFactor = Math.pow(10, this.numPrecision);
             // 将数字转成整数运算后在处理小数精度 解决小数运算问题
             return this.toPrecision((precisionFactor * value + precisionFactor * step) / precisionFactor);
@@ -175,7 +251,7 @@ export default {
         // 减
         reduce() {
             // 如果为禁止点击
-            if(this.disabled || this.reduceDisabled) return;
+            if(this.disabled || this.readonly || this.reduceDisabled) return;
             // 输入框的值
             const value = this.value || 0;
             // 进行加法运算
@@ -184,7 +260,9 @@ export default {
             this.setCurrentValue(newVal);
         },
         _reduce(value, step) {
+            // 如果值不为数字且不为空
             if (typeof value !== 'number' && value !== undefined) return this.currentValue;
+            // 先将值乘以10的小数点位数幂次方
             const precisionFactor = Math.pow(10, this.numPrecision);
             // 将数字转成整数运算后在处理小数精度 解决小数运算问题
             return this.toPrecision((precisionFactor * value - precisionFactor * step) / precisionFactor);
@@ -196,33 +274,91 @@ export default {
             if (typeof newVal === 'number' && this.precision !== undefined) {
                 newVal = this.toPrecision(newVal, this.precision);
             }
-            // 处理最大值和最小值
-            if (newVal >= this.max) newVal = this.max;
-            if (newVal <= this.min) newVal = this.min;
-            if (oldVal === newVal) return;
+            // 如果值不为空
+            if (newVal !== null) {
+                // 处理最大值和最小值
+                if (newVal >= this.max) newVal = this.max;
+                if (newVal <= this.min) newVal = this.min;
+                // 处理输入框输入大于max或小于min重新设置值的问题 判断formatterValue 是为了防止格式化错误
+                if (oldVal === newVal) {
+                    this.$refs.input.value = this.formatterValue;
+                    return;
+                }
+            }
 
+            this.currentValue = newVal;
             this.$emit('input', newVal);
             this.$emit('on-change', newVal, oldVal);
-            this.currentValue = newVal;
-        },
-        // 点击事件
-        handleClick(e) {
         },
         // 获取精度小数位数
         getPrecision(value) {
-          if (value === undefined) return 0;
-          const valueString = value.toString();
-          const dotPosition = valueString.indexOf('.');
-          let precision = 0;
-          if (dotPosition !== -1) {
-            precision = valueString.length - dotPosition - 1;
-          }
-          return precision;
+            // 小数点位数
+            let precision = 0;
+            // 如果当前值存在 进行计算 否则直接返回 0
+            if(!isNull(value)) {
+                // 获取小数点位置
+                const valueString = value.toString();
+                const dotPosition = valueString.indexOf('.');
+                // 如果存在小数点
+                if (dotPosition !== -1) {
+                    // 计算小数位数
+                    precision = valueString.length - dotPosition - 1;
+                }
+            }
+            return precision;
         },
         // 按照精度转换值
         toPrecision(num, precision) {
-          if (precision === undefined) precision = this.numPrecision;
-          return parseFloat(parseFloat(Number(num).toFixed(precision)));
+            if (precision === undefined) precision = this.numPrecision;
+            return parseFloat(parseFloat(Number(num).toFixed(precision)));
+        },
+        // 输入框值改变事件
+        handleChange(event) {
+            // 获取输入框的值
+            let val = event.target.value.trim();
+
+            // 转回数据的方法存在
+            if (this.parser) {
+                val = this.parser(val);
+            }
+            // 如果为空字符
+            if(val.length === 0){
+                this.setCurrentValue(null);
+                return;
+            }
+            // 将字符串转成数字
+            val = Number(val);
+
+            // 如果是数字 进行设置
+            if (!isNaN(val)) {
+                this.setCurrentValue(val);
+            }
+            else {
+                event.target.value = this.formatterValue;
+            }
+        },
+        // 处理键盘上下键按下事件
+        handleKeyDown (e) {
+            // Up（上箭头）
+            if (e.keyCode === 38) {
+                // 组织默认事件
+                e.preventDefault();
+                // 加
+                this.plus();
+            }
+            // Down（下箭头）
+            else if (e.keyCode === 40) {
+                // 组织默认事件
+                e.preventDefault();
+                // 减
+                this.reduce();
+            }
+        },
+        handleFocus(event) {
+          this.$emit('on-focus', event);
+        },
+        handleBlur(event) {
+          this.$emit('on-blur', event);
         },
     }
 };
--- test/views/input-number.vue
+++ test/views/input-number.vue
@@ -13,21 +13,15 @@
 }
 .demo-item-box {
     margin-bottom: 15px;
-}
-.demo-diy-item {
-    .m-ipt-wrap {
-        margin-right: 10px;
+
+    &.inline {
+        .m-ipt-number-wrap {
+            margin-right: 20px;
+            margin-bottom: 10px;
+        }
     }
 }
 
-.demo-diy-item-2 {
-    .m-ipt-wrap {
-        width: 48%;
-    }
-    .m-ipt-wrap + .m-ipt-wrap  {
-        margin-left: 3.5%
-    }
-}
 
 </style>
 <template>
@@ -38,7 +32,105 @@
             </div>
             <div class="demo-item-cont">
                 <div class="demo-item-box">
-                    <m-input-number v-model="value0" :min="-5" :max="10" placeholder="提示信息" />
+                    <m-input-number v-model="value0" :min="0" :max="5" placeholder="提示信息" />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>尺寸和形状</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value7" :min="0" :max="10" size="mini" />
+                    <m-input-number v-model="value7" :min="0" :max="10" size="mini" shape="fillet" />
+                </div>
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value7" :min="0" :max="10" size="small" />
+                    <m-input-number v-model="value7" :min="0" :max="10" size="small" shape="fillet" />
+                </div>
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value7" :min="0" :max="10"  />
+                    <m-input-number v-model="value7" :min="0" :max="10" shape="fillet" />
+                </div>
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value7" :min="0" :max="10" size="large" />
+                    <m-input-number v-model="value7" :min="0" :max="10" size="large" shape="fillet" />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>按钮位置</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value7" controls-position="right" :min="0" :max="10" shape="fillet" size="mini" />
+                    <m-input-number v-model="value7" controls-position="right" :min="0" :max="10" shape="fillet" size="small" />
+                    <m-input-number v-model="value7" controls-position="right" :min="0" :max="10" shape="fillet" />
+                    <m-input-number v-model="value7" controls-position="right" :min="0" :max="10" shape="fillet" size="large" />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>占整行</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box">
+                    <m-input-number v-model="value7" long />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>步数与精度</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box">
+                    <m-input-number v-model="value1" :step="1.25" :precision="2" :min="1" :max="10" />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>格式化</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box inline">
+                    <m-input-number
+                        v-model="value3_1"
+                        :min="0"
+                        :max="10000"
+                        :formatter="formatter_1"
+                        :parser="parser_1" />
+
+                    <!-- 百分比 -->
+                    <m-input-number
+                        v-model="value3_2"
+                        :min="0"
+                        :max="100"
+                        :formatter="formatter_2"
+                        :parser="parser_2"
+                         />
+                </div>
+            </div>
+        </div> <!-- demo-item 结束 -->
+
+
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>只读和不可编辑状态</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box inline">
+                    <m-input-number v-model="value6" :editable="false" />
+                    <m-input-number v-model="value6" readonly />
                 </div>
             </div>
         </div> <!-- demo-item 结束 -->
@@ -49,24 +141,23 @@
             </div>
             <div class="demo-item-cont">
                 <div class="demo-item-box">
-                    <m-input-number v-model="value1" disabled />
+                    <m-input-number v-model="value4" :disabled="disabled" />
+                    <m-button type="primary" @click="toggle">切换禁止状态</m-button>
                 </div>
             </div>
         </div> <!-- demo-item 结束 -->
 
         <div class="demo-item demo-diy-item">
             <div class="demo-item-tit">
-                <h3>步数</h3>
+                <h3>事件处理</h3>
             </div>
             <div class="demo-item-cont">
                 <div class="demo-item-box">
-                    <m-input-number v-model="value2" :step="1.25" :precision="2" :min="1" :max="10" />
+                    <m-input-number v-model="value5" :min="0" :max="99" @on-change="onChange" @on-focus="onFocus" @on-blur="onBlur" />
+                    <m-button type="primary" @click="getValue">获取当前的值</m-button>
                 </div>
             </div>
         </div> <!-- demo-item 结束 -->
-
-
-        <m-button type="primary" @click="getValue">点击</m-button>
 
 
     </div> <!-- demo-wrap 结束 -->
@@ -75,9 +166,17 @@
 export default {
     data() {
         return {
-            value0: 12,
+            value0: 1,
             value1: 1,
-            value2: 1
+            value2: 1,
+            value3_1: 1100,
+            value3_2: 10,
+            value4: 99,
+            value5: 1,
+            value6: 10,
+            value7: 8,
+            disabled: true,
+            editable: false
         }
     },
     watch: {
@@ -88,7 +187,38 @@ export default {
     },
     methods: {
         getValue() {
-            console.log(this.value0)
+            console.log(this.value3_1)
+        },
+        toggle() {
+            this.disabled = !this.disabled;
+        },
+        toggleEditable() {
+            this.editable = !this.editable;
+        },
+        onChange(newVal, oldVal) {
+            console.log("newVal：" + newVal + "  oldVal：" + oldVal)
+        },
+        onFocus(e) {
+            console.log("输入框获取了焦点")
+            console.log(e)
+        },
+        onBlur(e) {
+            console.log("输入框失去了焦点")
+            console.log(e)
+        },
+        formatter_1(value) {
+            // /(\d)(?=(?:\d{3})+$)/g
+            return `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+        },
+        parser_1(value) {
+            return value.replace(/\$\s?|(,*)/g, '');
+        },
+        formatter_2(value) {
+            return `${value}%`;
+        },
+        parser_2(value) {
+            return value.replace('%', '');
         }
     }
 }
