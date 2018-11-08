18847e18e8bfecde923421218266bd01b66534ff
chenyuebin <568474678@qq.com>
2018-11-08 12:33:33 +0800

    添加dom事件绑定方法 on、off

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,3 +1,7 @@
+/* istanbul ignore next */
+import Vue from 'vue';
+const isServer = Vue.prototype.$isServer;
+
 /**
  * DOM 操作方法
  */
@@ -60,6 +64,40 @@ export function removeClass(el, cls) {
         el.className = trim(curClass);
     }
 };
+
+/* istanbul ignore next */
+export const on = (function() {
+    if (!isServer && document.addEventListener) {
+        return function(element, event, handler) {
+            if (element && event && handler) {
+                element.addEventListener(event, handler, false);
+            }
+        };
+    } else {
+        return function(element, event, handler) {
+            if (element && event && handler) {
+                element.attachEvent('on' + event, handler);
+            }
+        };
+    }
+})();
+
+/* istanbul ignore next */
+export const off = (function() {
+    if (!isServer && document.removeEventListener) {
+        return function(element, event, handler) {
+            if (element && event) {
+                element.removeEventListener(event, handler, false);
+            }
+        };
+    } else {
+        return function(element, event, handler) {
+            if (element && event) {
+                element.detachEvent('on' + event, handler);
+            }
+        };
+    }
+})();
 
 // 将属性转换成驼峰
 function camelCase(str){
