13da2383504ef965a0929b92a03fa005f11472da
chenyuebin <568474678@qq.com>
2018-11-08 12:17:52 +0800

    返回顶部方法测试

--- examples/views/test_popper.vue
+++ examples/views/test_popper.vue
@@ -19,6 +19,12 @@
         margin-bottom: 15px;
     }
 }
+
+.back-top {
+    position: fixed;
+    right: 20px;
+    bottom: 200px;
+}
 </style>
 <template>
     <div class="demo-wrap">
@@ -34,16 +40,40 @@
                     </div>
                 </div>
             </div>
-            <svg class="circular" viewBox="25 25 50 50">
-                                   <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke-miterlimit="10"></circle>
-                               </svg>
+        </div> <!-- demo-item 结束 -->
+
+        <div style="height:2000px">
+
+        </div>
+        <div class="demo-item demo-diy-item">
+            <div class="demo-item-tit">
+                <h3>返回顶部</h3>
+            </div>
+            <div class="demo-item-cont">
+                <div class="demo-item-box">
+                    <m-button class="back-top" @click="scrollTop" bordered>点击</m-button>
+                </div>
+            </div>
         </div> <!-- demo-item 结束 -->
 
     </div> <!-- demo-wrap 结束 -->
 </template>
 <script>
+import { scrollTop } from '@/utils/dom.js';
 const Popper = Vue.prototype.$isServer ? function() {} : require('popper.js');
 
+function getTime() {
+    var date = new Date();
+    date.year = date.getFullYear();
+    date.month = date.getMonth() + 1;
+    date.date = date.getDate();
+    date.day = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")[date.getDay()];
+    date.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    date.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    date.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+    return "现在是:" + date.year + "年" + date.month + "月" + date.date + "日 " + date.hour + ":" + date.minute + ":" + date.second
+}
 
 export default {
     data() {
@@ -53,9 +83,9 @@ export default {
     mounted() {
         var reference = document.querySelector('#btn');
         var popper = document.querySelector('#popper');
-        console.log(reference)
-        console.log(popper)
-        console.log(Popper);
+        // console.log(reference)
+        // console.log(popper)
+        // console.log(Popper);
         // var anotherPopper = new Popper.Popper(reference, popper, {
         //     placement: 'left'
         // });
@@ -65,7 +95,13 @@ export default {
         handleClose(e) {
             console.log(e)
         },
-
+        scrollTop() {
+            const sTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+            console.log(getTime())
+            scrollTop(window, sTop, 0, 1000, function() {
+                console.log(getTime())
+            });
+        }
     },
 }
 </script>
